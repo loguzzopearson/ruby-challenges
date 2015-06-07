c49565ca72e47aa02992a02142d659fbd0435694
Christie Pearson <hello@psdworkshop.com>
2015-06-07 21:27:18 +0200

    Final projects for 104

--- blog.rb
+++ blog.rb
@@ -1,53 +1,56 @@
 class Blog
-	attr_accessor :author, :time, :title, :all_blog_posts
+	attr_accessor :author, :time_created, :title, :all_blog_posts
 
+#initialize to allow adding title, content to post
 	def initialize
-		@author = author
-		@time = Time.now
+		@author = "Jack Kerouac"
+		@time_created = Time.now
 		puts "Enter name of blog"
 		@title = gets.chomp
-		@all_blog_posts = {}
+		@all_blog_posts = []
 		@nr_blog_posts = 0
 	end	
 
 	def create_blogpost
 		new_post = Blog_Post.new
-		@all_blog_posts << new_post
 		@nr_blog_posts +=1
 	end
 	
 	def collect_blog_posts
-		return @all_blog_posts
+		return @all_blog_posts.sort
 	end
 		
 	def publish(all_blog_posts)
 		all_blog_posts.each do |blog_post|
 		print blog_post.author
-		print blog_post.time
+		print blog_post.time_created
 		print blog_post.title
 		print blog_post.content
 	end
-
+end
 end
 
 class Blog_Post 
-attr_accessor :author, :time, :title, :content
+attr_accessor :time_created, :title, :content
 
-	def intialize
-		@author = "E A Poe"
-		@time = Time.now
-		puts "Enter blog title"
+	def initialize
+		@time_created = Time.now
+		puts "Enter post title"
 		@title = gets.chomp
+		
 		puts "Write your post"
 		@content = gets.chomp
 	end
-end
 end
 
 # Create Objects
 simple_blog = Blog.new
 blogpost1 = simple_blog.create_blogpost
+all_blog_posts = simple_blog.collect_blog_posts
+puts simple_blog.inspect
+simple_blog.publish(all_blog_posts)
 
-# Call Methods
-simple_blog.publish
-simple_blog.sort
+blogpost2 = simple_blog.create_blogpost
+all_blog_posts = simple_blog.collect_blog_posts
+puts simple_blog.inspect
+simple_blog.publish(all_blog_posts)
